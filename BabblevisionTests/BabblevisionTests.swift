dea1bb13d7fc5feafc8c8823120324759b35b1f8
Andrew Hao <andrewhao@gmail.com>
2020-05-22 16:33:56 -0700

    Initial Commit

---
+++ BabblevisionTests/BabblevisionTests.swift
@@ -0,0 +1,34 @@
+//
+//  BabblevisionTests.swift
+//  BabblevisionTests
+//
+//  Created by Andrew Hao on 5/22/20.
+//  Copyright © 2020 g9Labs. All rights reserved.
+//
+
+import XCTest
+@testable import Babblevision
+
+class BabblevisionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
